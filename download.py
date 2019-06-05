15d2fadf16def89ccade817c19011a9df9246e86
orrpeles <orr.backup37@gmail.com>
2019-06-05 16:56:53 +0300

    added relevant OS features to fix issues with ~ relevant to bash

--- download.py
+++ download.py
@@ -1,9 +1,14 @@
 #!/bin/python
 # Get audio from YouTube
 # Ref: https://stackoverflow.com/questions/49204113/download-audio-from-youtube-using-pytube
+# ref as to OS: https://stackoverflow.com/questions/2057045/pythons-os-makedirs-doesnt-understand-in-my-path
 
 from pytube import YouTube
+import os
+
 yt = YouTube(str(input("Enter the link you wish to download: \n")))
 t = yt.streams.filter(only_audio=True).all()
-t[0].download('~/Music/')
+my_dir = os.path.expanduser('~/Music/')
+t[0].download(my_dir)
+
 
